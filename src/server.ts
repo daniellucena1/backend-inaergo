3de2c90f6bac0b8662822c46a13dc138fd385824
rbsilva1 <rodrigo.belarmino17@gmail.com>
2025-02-01 18:43:34 -0300

    fix: add cors

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import authRoutes from './routes/authRoute'
 import adminRoutes from './routes/adminRoute'
 import employeeRoutes from './routes/employeeRoute';
 import managerRoutes from './routes/managerRoute';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use(authRoutes, adminRoutes, employeeRoutes, managerRoutes);
 
