e19daf701a96f8f299a2fd8db69566a261630e99
daniellucena1 <daniellucena.jt@gmail.com>
2025-02-15 23:42:25 -0300

    fix: adicionando autenticação de token nas rotas de respostas

--- src/routes/answerRoute.ts
+++ src/routes/answerRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { answerController } from "../controllers/answerController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.post("/answer", answerController.createAnswer);
-router.get("/answer", answerController.getAnswer);
-router.get("/answer/:id", answerController.getAnswerByEmployeeId);
+router.post("/answer", authMiddleware.authenticate, answerController.createAnswer);
+router.get("/answer", authMiddleware.authenticate, authMiddleware.isManager, answerController.getAnswer);
+router.get("/answer/:id", authMiddleware.authenticate, authMiddleware.isManager, answerController.getAnswerByEmployeeId);
 
 export default router;
