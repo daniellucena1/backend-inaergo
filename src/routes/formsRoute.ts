64281a79ae0fb218f3a618a2ebb3dcee1181eb57
daniellucena1 <daniellucena.jt@gmail.com>
2025-02-17 00:27:13 -0300

    fix: mudando a verificação da rota para o formulário

--- src/routes/formsRoute.ts
+++ src/routes/formsRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { formsController } from "../controllers/formsController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.post("/forms", formsController.createForm);
-router.get("/forms", formsController.getForm);
+router.post("/forms", authMiddleware.authenticate, authMiddleware.isManager, formsController.createForm);
+router.get("/forms", authMiddleware.authenticate, authMiddleware.isManager, formsController.getForm);
 
 export default router;
